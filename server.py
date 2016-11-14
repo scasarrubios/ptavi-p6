0cb1a1b530247f6bd63d3cb5d220983c6a0865a6
Sergio Casarrubios De Arcos <scasarru@gamma08.aulas.gsyc.urjc.es>
2016-11-14 10:58:22 +0100

    Excepciones implementadas

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -15,17 +16,16 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.wfile.write(b"Hemos recibido tu peticion")
-        while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
-
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
+        line = self.rfile.read().decode('utf-8').split()
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
-    serv.serve_forever()
+    try:
+        serv = socketserver.UDPServer((sys.argv[1], int(sys.argv[2])), EchoHandler)
+        print("Lanzando servidor UDP de eco...")
+    except IndexError:
+        sys.exit("Usage: python server.py IP port audio_file")
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print("\nFinalizado servidor")
