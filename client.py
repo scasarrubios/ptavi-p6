e2849e5b892fa5cd63d9689d5a73a2f3c355b207
Sergio Casarrubios <scasarrubios@virtualbox.org>
2016-11-16 00:20:25 +0100

    cliente aparentemente terminado

--- client.py
+++ client.py
@@ -5,25 +5,32 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
-# Cliente UDP simple.
-
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
-
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+try:
+    SERVER = sys.argv[2].split('@')[-1].split(':')[0]
+    PORT = int(sys.argv[2].split(':')[-1])
+    RECEIVER = sys.argv[2].split('@')[0]
+    method = sys.argv[1].upper()
+except:
+    sys.exit('Usage: client.py method receiver@ip:SIPport')
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
-print("Enviando: " + LINE)
-my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-data = my_socket.recv(1024)
-
+line = method + ' sip:' + RECEIVER + '@' + SERVER + ' SIP/2.0'
+print("Enviando: " + line)
+my_socket.send(bytes(line, 'utf-8') + b'\r\n\r\n')
+answer = my_socket.recv(1024)
+template = ('SIP/2.0 100 Trying\r\n\r\n'
+            'SIP/2.0 180 Ring\r\n\r\n'
+            'SIP/2.0 200 OK\r\n\r\n')
+if answer == template:
+    method = 'ACK'
+    line = method + ' sip:' + RECEIVER + '@' + SERVER + ' SIP/2.0'
+    my_socket.send(bytes(line, 'utf-8') + b'\r\n\r\n')
 print('Recibido -- ', data.decode('utf-8'))
 print("Terminando socket...")
 
